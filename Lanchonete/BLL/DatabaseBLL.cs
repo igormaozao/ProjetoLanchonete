772f1c5f7d0c325ee2c69a7d3ce665d85a01dbf4
Igor Maozao <igormaozao@gmail.com>
2018-04-23 11:48:42 -0300

    - Alterado a variável do DatabaseBLL que era static. - O static estava causando os objetos que eram obtidos das listas do "banco de dados" a manter sua referência com a List<T>, mesmo os objetos sendo "clonados" como .Clone()

--- Lanchonete/BLL/DatabaseBLL.cs
+++ Lanchonete/BLL/DatabaseBLL.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Lanchonete.BLL {
     public abstract class DatabaseBLL {
 
-        protected static Database Database { get; private set; }
+        protected Database Database { get; private set; }
 
         protected DatabaseBLL() {
             if (Database == null) {
